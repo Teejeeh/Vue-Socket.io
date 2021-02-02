68d8d3133d18ba53db7534abc89d66a54c9c359b
Thomas Huisman <teejeeh@gmail.com>
2021-02-02 09:59:43 +0100

    Ugh

--- src/index.js
+++ src/index.js
@@ -26,18 +26,10 @@ export default class VueSocketIO {
      * Vue.js entry point
      * @param Vue
      */
-    install(Vue) {
-        const version = Number(Vue.version.split('.')[0])
-
-        if (version >= 3) {
-            Vue.config.globalProperties.$socket = this.io;
-            Vue.config.globalProperties.$vueSocketIo = this;
-        } else {
-            Vue.prototype.$socket = this.io;
-            Vue.prototype.$vueSocketIo = this;
-        }
-
-        Vue.mixin(Mixin);
+    install(app, options) {
+        app.config.globalProperties.$socket = this.io;
+        app.config.globalProperties.$vueSocketIo = this;
+        app.mixin(Mixin);
 
         Logger.info('Vue-Socket.io plugin enabled');
     }
